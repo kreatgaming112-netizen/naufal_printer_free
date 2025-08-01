5fa92f52701ced975b9446d30de456b5dc5631d8
NaufalGG <kreatgaming112@gmail.com>
2025-08-01 14:06:53 +0700

    config.lua
    1

---
+++ config.lua
@@ -0,0 +1,26 @@
+
+Config = {}
+
+Config.Locale = 'id' -- 'en', 'id', 'fr', 'de', 'es', 'it', 'pt', 'tr' -- Bahasa
+
+Config.Debug = false
+
+Config.Notify = 'ox' --ox, esx, okok,qb,wasabi,custom
+
+Config.CheckItem = false -- Jika Anda ingin pemain memiliki item sebelum membukanya
+
+Config.PrinterModel = {`prop_printer_02`,`prop_printer_01`}
+
+Config.ItemName = 'paper'
+
+Config.Print = {
+    Price = 35000, -- Harga
+    Account = 'money' -- Ini adalah akun yang akan dipotong uang Anda.
+}
+
+Config.EnableLocation = false
+
+Config.Locations = {
+    {coords = vector3(369.87, -1584.47, 29.18), heading = 190.0, object = "prop_printer_02"},
+}
+
