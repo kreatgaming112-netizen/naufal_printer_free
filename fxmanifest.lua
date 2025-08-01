445d23926b8c40390bc529025fc1ae85146656bd
NaufalGG <kreatgaming112@gmail.com>
2025-08-01 14:08:12 +0700

    Create fxmanifest.lua
    fx

---
+++ fxmanifest.lua
@@ -0,0 +1,34 @@
+fx_version 'cerulean'
+games { 'gta5' }
+lua54 'yes'
+
+name 'Printer Scripts By NaufalGG'
+author 'NaufalGG'
+version '1.0.4'
+
+ui_page 'web/index.html'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua',
+    'locales/locale.lua'
+}
+
+client_scripts {
+    'client/main.lua',
+    'client/bridge/*'
+}
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua',
+    'server/bridge/*'
+}
+
+dependencies {
+    'ox_lib',
+}
+
+files {
+    'web/index.html',
+    'locales/*.json'
+}
